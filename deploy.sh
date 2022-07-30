9b743dd788af3171cc48319a7d7b4bfa14f22b85
jean balangue <jeanbalangue@hotmail.fr>
2022-07-30 20:36:23 +0200

    mise en forme

--- deploy.sh
+++ deploy.sh
@@ -9,14 +9,14 @@
 ####################################################################
 function show_help(){
   echo "
+  Script basique de déploiement de conteneurs réalisé via xavki sur youtube
   Options :
   - --help : affiche l'aide
-  - --create : lancer des conteneurs arg:number:nbre de conteneur
+  - --create : lancer des conteneurs docker arg:number:nbre de conteneur
   - --drop : supprimer les conteneurs créer par deploy.sh
-  - --info : caractéristiques des conteneurs (ip,nom,user ...)
+  - --ip : ip des conteneurs
   - --start : redémarrage des conteneurs du script
   - --stop : arrête les conteneurs du script
-  - --ansible : déploiement arborescence ansible
   "
 }
 
@@ -26,7 +26,7 @@ if [ "$1" == "--help" ];then
 
 elif [ "$1" == "--create" ];then
   echo ""
-  echo "L'option choisie est "$1
+  echo "L'option choisie est $1"
   echo ""
   # test pour savoir si un second argument est présent
   # pas d'espace pour définir une variable
@@ -34,14 +34,13 @@ elif [ "$1" == "--create" ];then
   [ "$2" != "" ] && nb_machines=$2
 
   # recuperation de l'id max, awk -F pr definir separateur, -v pour def une variable
+  # le format, c'est du template go ...
   min=1
   max=0
   idmax=`docker ps -a --format '{{ .Names }}' | awk -F "-" -v user=$USER '$0 ~ user"-alpine" {print $3}' | sort -r | head -1`
   echo "idmax : ${idmax}"
   min=$(($idmax+1))
-  max=$(($min+$nb_machines))
-  # pour test sans docker on supprime 1 pour cohérence
-  max=$(($max-1))
+  max=$(($min+$nb_machines-1))
 
   # On boucle sur le nbre de machines a faire
   for i in $(seq $min $max);do
@@ -52,7 +51,7 @@ elif [ "$1" == "--create" ];then
 
 elif [ "$1" == "--drop" ];then
   echo ""
-  echo "L'option choisie est "$1
+  echo "L'option choisie est $1"
   echo ""
   echo "docker rm -f $USER-alpine"
   echo 'Drop ts les conteneurs jean-alpine'
@@ -60,9 +59,9 @@ elif [ "$1" == "--drop" ];then
   echo "recupere les id \$1 présent en 1ere colonne de docker ps des conteneurs s'appelant $USER-alpine"
   docker rm -f $(docker ps -a | grep $USER-alpine | awk '{print $1}')  
 
-elif [ "$1" == "--info" ];then
+elif [ "$1" == "--ip" ];then
   echo ""
-  echo "L'option choisie est "$1
+  echo "L'option choisie est $1"
   echo ""
   for conteneur in $(docker ps -a | grep $USER-alpine | awk '{print $1}');do
     docker inspect --format='{{.Name}} -- {{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' $conteneur
@@ -70,20 +69,15 @@ elif [ "$1" == "--info" ];then
 
 elif [ "$1" == "--start" ];then
   echo ""
-  echo "L'option choisie est "$1
+  echo "L'option choisie est $1"
   echo ""
   docker start $(docker ps -a | grep $USER-alpine | awk '{print $1}')
 
 elif [ "$1" == "--stop" ];then
   echo ""
-  echo "L'option choisie est "$1
+  echo "L'option choisie est $1"
   echo ""
   docker stop $(docker ps -a | grep $USER-alpine | awk '{print $1}')
-
-elif [ "$1" == "--ansible" ];then
-  echo ""
-  echo "L'option choisie est "$1
-  echo ""
 
 else
   show_help
