677b959d49f1d554ee95fd071ad0af85d2ffae64
jean balangue <jeanbalangue@hotmail.fr>
2022-07-30 13:33:06 +0200

    dev : ajout condition if else

--- deploy.sh
+++ deploy.sh
@@ -6,11 +6,46 @@
 # date : 30/07/2022
 #
 ####################################################################
+
+if [ "$1" == "--help" ];then
+# $1 est le 1er arg du script
+echo ""
+echo "help! tape juste le nom de la commande pour voir l'aide"
+echo ""
+
+elif [ "$1" == "--create" ];then
+echo ""
+echo "L'option choisie est "$1
+echo ""
+
+elif [ "$1" == "--drop" ];then
+echo ""
+echo "L'option choisie est "$1
+echo ""
+
+elif [ "$1" == "--info" ];then
+echo ""
+echo "L'option choisie est "$1
+echo ""
+
+elif [ "$1" == "--start" ];then
+echo ""
+echo "L'option choisie est "$1
+echo ""
+
+elif [ "$1" == "--ansible" ];then
+echo ""
+echo "L'option choisie est "$1
+echo ""
+
+else
 echo "
 Options :
+ - --help : affiche l'aide
  - --create : lancer des conteneurs
  - --drop : supprimer les conteneurs créer par deploy.sh
- - --infos : caractéristiques des conteneurs (ip,nom,user ...)
+ - --info : caractéristiques des conteneurs (ip,nom,user ...)
  - --start : redémarrage des conteneurs
  - --ansible : déploiement arborescence ansible
 "
+fi
